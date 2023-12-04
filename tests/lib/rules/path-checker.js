7e1f1ab7e2b1936d362c75a52377e69b403b0633
Zayka Julia <zaykajulias@gmail.com>
2023-12-04 13:18:31 +0300

    add alias

--- tests/lib/rules/path-checker.js
+++ tests/lib/rules/path-checker.js
@@ -16,16 +16,32 @@ const rule = require("../../../lib/rules/path-checker"),
 // Tests
 //------------------------------------------------------------------------------
 
-const ruleTester = new RuleTester();
+const ruleTester = new RuleTester({
+parserOptions: {ecmaVersion: 6, sourceType: "module"}
+});
 ruleTester.run("path-checker", rule, {
   valid: [
     // give me some code that won't trigger a warning
+     {
+      filename: 'C:\\Users\\username\\Documents\\production-progect\\src\\entities\\Article',
+      code: "import { addNewCommentFormActions, addNewCommentFormReducer } from '../../model/slices/addNewCommentFormSlice'",
+      errors: [{ message: "В рамках одного слайса все пути должны быть относительными" }],
+    },
   ],
 
   invalid: [
     {
-      code: "asdsdg",
-      errors: [{ message: "Fill me in.", type: "Me too" }],
+      filename: 'C:\\Users\\username\\Documents\\production-progect\\src\\entities\\Article',
+      code: "import { addNewCommentFormActions, addNewCommentFormReducer } from '@/entities/Article/addNewCommentForm/model/slices/addNewCommentFormSlice'",
+      errors: [{ message: "В рамках одного слайса все пути должны быть относительными" }],
+      options: [{
+        alias: "@"
+      }]
+    },
+    {
+      filename: 'C:\\Users\\username\\Documents\\production-progect\\src\\entities\\Article',
+      code: "import { addNewCommentFormActions, addNewCommentFormReducer } from 'entities/Article/addNewCommentForm/model/slices/addNewCommentFormSlice'",
+      errors: [{ message: "В рамках одного слайса все пути должны быть относительными" }],
     },
   ],
 });
